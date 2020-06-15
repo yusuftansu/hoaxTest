8ac420c7178fccce45a5079051bfae02fb002768
yusuftansu <yusuftansu@hotmail.com>
2020-06-16 01:04:00 +0300

    backend-first commit

---
+++ ws/src/main/java/com/hoaxify/ws/user/UniqueUsernameValidator.java
@@ -0,0 +1,25 @@
+package com.hoaxify.ws.user;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername,	String>{
+
+	@Autowired 
+	UserRepository userRepository;
+	
+	@Override
+	public boolean isValid(String username, ConstraintValidatorContext context) {
+		User user = userRepository.findByUsername(username);
+		if(user != null) {
+			System.out.println("girrrrr");
+			return false;
+		}
+		System.out.println("asdlkasldaksş");
+		return true;
+	}
+
+}
+  
